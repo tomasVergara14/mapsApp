2bc1abb06b09db42cdf8d069cd16955aeee628ec
Tom <tomasvergara272@gmail.com>
2023-10-15 20:50:08 -0300

    sideMenu items

--- src/app/maps/components/side-menu/side-menu.component.ts
+++ src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+  name: string;
+  route: string;
+}
+
 @Component({
   selector: 'maps-side-menu',
   templateUrl: './side-menu.component.html',
@@ -8,4 +13,10 @@ import { Component } from '@angular/core';
 })
 export class SideMenuComponent {
 
+  public menuItems: MenuItem[] = [
+    { route: '/maps/fullscreen', name: 'FullScreen'},
+    { route: '/maps/zoom-range', name: 'ZoomRange'},
+    { route: '/maps/markers', name: 'Markers'},
+    { route: '/maps/properties', name: 'Properties'},
+  ] 
 }
