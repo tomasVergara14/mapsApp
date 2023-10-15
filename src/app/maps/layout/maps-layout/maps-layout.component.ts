1c13064b0c46b0aa9b203319c8b29d8ad93cc6ca
Tom <tomasvergara272@gmail.com>
2023-10-15 20:31:38 -0300

    Maps App

---
+++ src/app/maps/layout/maps-layout/maps-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-maps-layout',
+  templateUrl: './maps-layout.component.html',
+  styles: [
+  ]
+})
+export class MapsLayoutComponent {
+
+}
---
+++ src/app/maps/pages/properties-page/properties-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './properties-page.component.html',
+  styleUrls: ['./properties-page.component.scss']
+})
+export class PropertiesPageComponent {
+
+}
