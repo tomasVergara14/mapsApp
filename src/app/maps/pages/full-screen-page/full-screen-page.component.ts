d043693ca0d2f41754946696d32c2df30854ab54
Tom <tomasvergara272@gmail.com>
2023-10-16 15:09:53 -0300

    npm i mapbox

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+
+import * as mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+(mapboxgl as any).accessToken = 'pk.eyJ1IjoidG90b3ZlcmdhcmEiLCJhIjoiY2xuczVydjFjMDhwdTJ0bWh4M3A1dmlrZiJ9.J5-_UJPm3lMtIG6wmkCm3Q';
 
 @Component({
   selector: 'app-full-screen-page',
   templateUrl: './full-screen-page.component.html',
-  styles: [
-  ]
+  styleUrls: ['./full-screen-page.component.scss']
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit{
+
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+      });
+  }
 
 }
