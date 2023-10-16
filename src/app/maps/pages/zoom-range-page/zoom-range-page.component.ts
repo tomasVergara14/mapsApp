1e48a2632a86a67bca2bc84aa5e2e02e8120c568
Tom <tomasvergara272@gmail.com>
2023-10-16 17:06:58 -0300

    zoom control

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -11,17 +11,48 @@ export class ZoomRangePageComponent implements AfterViewInit {
 
   @ViewChild('map')
   public divMap?: ElementRef;
+  public currentZoom: number = 10;
+  public map?: Map;
+
 
   ngAfterViewInit(): void {
 
     if(!this.divMap) throw 'el elemento HTML no fue encontrado'
 
-    const map = new Map({
+    this.map = new Map({
       container: this.divMap?.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      zoom: this.currentZoom, // starting zoom
       });
+
+      this.mapListeners(); 
+  }
+
+  mapListeners(){
+    if( !this.map ) throw 'Map does not exist'
+
+    this.map.on('zoom', (ev)=>{
+      this.currentZoom = this.map!.getZoom();
+    })
+
+    this.map.on('zoomend', (ev)=>{
+      if( this.map!.getZoom() < 18 )return;
+      this.map!.zoomTo(18);
+    })
+  }
+
+  zoomIn(){
+    this.map?.zoomIn();
+  }
+
+  zoomOut(){
+    this.map?.zoomOut();
+  }
+
+  zoomChanged( value: string ){
+    this.currentZoom = +value;
+    this.map?.zoomTo( this.currentZoom );
   }
 
 }
