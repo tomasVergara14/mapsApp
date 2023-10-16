dad0ff74694b1ea369c37a0a6995debc03567659
Tom <tomasvergara272@gmail.com>
2023-10-16 19:01:54 -0300

    markers style

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+import { LngLat, Map } from 'mapbox-gl'
 
 @Component({
   selector: 'app-markers-page',
   templateUrl: './markers-page.component.html',
-  styles: [
-  ]
+  styleUrls:['./markers-page.component.scss']
 })
 export class MarkersPageComponent {
+
+  @ViewChild('map')
+  public divMap?: ElementRef;
+  public map?: Map;
+  public lngLat: LngLat = new LngLat(-64.18,-31.41);
+
+
+  ngAfterViewInit(): void {
+
+    if(!this.divMap) throw 'el elemento HTML no fue encontrado'
+
+    this.map = new Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 10, // starting zoom
+      });
+
+  }
 
 }
